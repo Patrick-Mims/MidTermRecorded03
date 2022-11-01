ac6280a543e67b92dee153869b28e2d8cffb1202
Patrick Mims <patrickkevinmims@gmail.com>
2022-10-31 17:54:29 -0700

    Initial Commit

---
+++ MidTermRecorded03/MidTermRecorded03App.swift
@@ -0,0 +1,17 @@
+//
+//  MidTermRecorded03App.swift
+//  MidTermRecorded03
+//
+//  Created by JPL-ST-SPRING2022 on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct MidTermRecorded03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
